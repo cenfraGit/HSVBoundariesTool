de8b4576842ccf03aadb1c59ea25d850d88ee167
cenfraGit <161994865+cenfraGit@users.noreply.github.com>
2024-11-14 21:14:46 -0600

    appearance modifications

--- main.py
+++ main.py
@@ -12,6 +12,181 @@ from copy import copy, deepcopy
 import os
 import platform
 
+from wx.lib.embeddedimage import PyEmbeddedImage
+
+hue_image = PyEmbeddedImage(
+    b'iVBORw0KGgoAAAANSUhEUgAAB1cAAAC1CAIAAACMHeExAAAAAXNSR0IArs4c6QAAAARnQU1B'
+    b'AACxjwv8YQUAAAAJcEhZcwAADsQAAA7DAdpqmNwAACMNSURBVHhe7djT1nRdloXRkbbtzD9t'
+    b'27Zt27Zt27Zt27ZtGzN3rV1txYovdmS+FUd10Gcb9/C02TPdC5PaX9v+UwdoO2DqQKkDtx0k'
+    b'ddC2g6UOnjpE2yFTh2o7dOowqcO2HS51+NQR2o6YOlLbkVNHSR217Wipo7cdI3XM1LHajp06'
+    b'TttxU8dL7dd2/NQJ2k6YOlHqxG0nSZ207WSpk6dO0XbK1KnaTp06Teq0badLnT51hrYzps7U'
+    b'dubUWVJnbTtb6uxt50idM3WutnOnztN23tT5Uudvu0Dqgm0XSl04dZG2i6Yu1nbx1CVSl2y7'
+    b'VOrSbZdJXTZ1ubbLp66QumLblVJXbrtK6qqpq7VdPXWNtmumrpW6dtt1Utdtu17q+qkbtN0w'
+    b'daO2G6dukrpp281SN2+7ReqWqVu13Tp1m7bbpm6Xun3bHVJ3TN2p7c6pu7TdNXW31N3b7pG6'
+    b'Z9u9UvdO3aftvqn7td0/9YDUA9selHpw20NSD009rO3hqUe0PTL1qNSj2x6TemzqcW2PTz2h'
+    b'7YmpJ6We3PaU1FPbnpZ6euoZbc9MPavt2annpJ7b9rzU89tekHph6kVtL069pO2lqZelXt72'
+    b'itQr216VenXqNW2vTb0u9fq2N6Te2Pam1JtTb2l7a+ptbW9PvSP1zrZ3pd7d9p7Ue1Pva3t/'
+    b'6gNtH0x9KPXhto+kPtr2sdTHU59o+2TqU22fTn0m9dm2z6U+n/pC2xdTX2r7cuorqa+2fS31'
+    b'9bZvpL6Z+lbbt1Pfaftu6nup77f9IPXDth+lfpz6SdtPUz9r+3nqF6lftv0q9eu236R+m/pd'
+    b'2+9Tf0j9se1PqT+3/SX119Tf2v6e+kfbP1P/StX/7F+Vf1b+Ufl75W9tf638pfLnyp8qf6z8'
+    b'oe33ld9Vflv5TeXXbb+q/LLyi8rPKz9r+2nlJ5UfV35U+WHlB23fr3yv8t3KdyrfbvtW5ZuV'
+    b'b1S+Xvla5attX6l8ufKlyhcrX2j7fOVzlc9WPlP5dNunKp+sfKLy8crHKh9t+0jlw5UPVT5Y'
+    b'+UDb+yvvq7y38p7Kuyvvantn5R2Vt1feVnlr21sqb668qfLGyhvaXl95XeW1lddUXl15Vdsr'
+    b'K6+ovLzysspL215SeXHlRZUXVl7Q9vzK8yrPrTyn8uzKs9qeWXlG5emVp1We2vaUypMrT6o8'
+    b'sfKEyuPbHld5bOUxlUdXHtX2yMojKg+vPKzy0LaHVB5ceVDlgZUHVO7fdr/KfSv3qdy7cq+2'
+    b'e1buUbl75W6Vu1bu0nbnyp0qd6zcoXL7tttVblu5TeXWlVu13bJyi8rNKzer3LRyk7YbV25U'
+    b'uWHlBpXrt12vct3KdSrXrlyrcs22a1SuXrla5aqVq7RduXKlyhUrV6hcvu1ylctWLlO5dOVS'
+    b'lUu2XaJy8crFKhetXKTtwpULVS5YuUDl/JXztZ23cp7KuSvnqpyz7RyVs1fOVjlr5SxtZ66c'
+    b'qXLGyhkqp6+cru20ldNUTl05VeWUbaeonLxysspJKyepnLjtRJUTVk5QOX5lv7bjVY5bOU7l'
+    b'2JVjtR2zcozK0StHqxy1cpS2I1eOVDli5QiVw7cdrnLYymEqh64cqu2QlUNUDl45WOWglYO0'
+    b'HbhyoMoBKweo7L9tfzU1u28u9xzvXu4p23O5e7anZs/Z7s2egz03ew72XOs52HOtN1O92enN'
+    b'SPdCT3meC93zPLW553lu8xzmuc1zmOcqz2Geqzwnea7ynOTNHm/GuJd4ynAv8ZzhqcE9w3OD'
+    b'pwD3Bs8Bnus7B3iu75zeub5zeje7uxjdzeLOuZ1a23M7t3YKbW/tHNqpsj20c2XnxM6V7Ymd'
+    b'+jontvd1M66bZd3M6tzUKai9qXNQp5r2oPaaTimda9pTOnV0Tmnv6BTRuaM9opsF3czn3M4p'
+    b'nL2dm+HcrGZP5tTLOZm9l1Ms5172WG6WcjOTcyPnTM6N3Azk9jpOadys45zGsY49jb2OYxp7'
+    b'Hcc0znXcJ41zHf9rGucu7pPGuYtjGnsXxzT2LvY0jl2c07iXLk5R3KeLcxT36eIcxbGLPYpj'
+    b'F3sUexd3i+JUxM0oTkXcJ4pzEccojkXsURyLOEdxnyLOUfyvRZxyuFnEKYf7FLHncCxiz2Ev'
+    b'4pjDuYj75HAu4m45nFs45rC3cMxhb2HP4djCOYf7tHDO4T4tnEI4tHBbCPf5Xzef120hHN/W'
+    b'/1MIpwr+/wzh3MIdQtg/0f8cwrmFYwjnFv7XEI5/5+bT+R9C2N/NzV9z89EcQzi3cAzh3MIx'
+    b'hL2FYwjHz3IOYW9hD2Fv4eJDuflNbr6SYwh7C3sIewt7CHsLxxD2FvYQ9hb2EI4t3HwZF//F'
+    b'zWexh3C9hWMOF4u4+TIufo2Lj+NiIHsj+wc5NbJncrGUPZZzL+dY9l4uJnPxoVz8KXs7ez7H'
+    b'z7IXdDGivaOLKV38MntQF5s6ZrW/m4sfZ4/r2Nf+d46J7ZWdQztVdgztYms3f9CxuIvR7d3t'
+    b'z+iY3l7fxZd0scE9w/03nTLcS9xjPJW4x3jxQ+1J7lWektyr3MM8vqqL3+rmw7r4sy52uqe6'
+    b'13pKda/1YrDXXlgKTIEpMAWmwBSYAlNgCkyBKTAFpsAUeKccUmAKTIEp8P+2kAJTYApMgSkw'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUeFVcCkyBx6hSYApMgSkwBabAFJgCU2AK'
+    b'TIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBR6eXwpMgSkwBabAFJgCU2AKTIEpMAWm'
+    b'wBSYAlNgCjzluhebAlNgCkyBd3p7KTAFpsAUeLmOFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAF'
+    b'psAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYArf2UmAKTIEpMAWmwBSYAlNgCkyBKXD/'
+    b'NynwFMKhhdtCSIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWm'
+    b'wBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAewshBabAFJgCU2AKTIEpMAVeLzMFpsAUmAJT'
+    b'YApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAq+iSoEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApM'
+    b'gSkwBabAFJgCU2AKTIEpMAWmwBR4eH4pMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSnwlOte'
+    b'bApMgSkwBd7p7aXAFJgCU+DlOlJgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWm'
+    b'wBSYAlNgCkyBKTAFpsAUmAJTYArc2kuBKTAFpsAUmAJTYApMgSkwBabA/d+kwFMIhxZuCyEF'
+    b'psAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabA'
+    b'FJgCU2AKTIEpMAWmwBSYAu8thBSYAlNgCkyBKTAFpsAUeL3MFJgCU2AKTIEpMAWmwBSYAlNg'
+    b'CkyBKTAFpsAUmAJTYAq8iioFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyB'
+    b'KTAFpsAUmAJT4OH5pcAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwFOue7EpMAWmwBR4p7eX'
+    b'AlNgCkyBl+tIgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabA'
+    b'FJgCU2AKTIEpcGsvBabAFJgCU2AKTIEpMAWmwBSYAvd/kwJPIRxauC2EFJgCU2AKTIEpMAWm'
+    b'wBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAU'
+    b'mAJTYAq8txBSYApMgSkwBabAFJgCU+D1MlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AK'
+    b'TIEp8CqqFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIGH'
+    b'55cCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJPue7FpsAUmAJT4J3eXgpMgSkwBV6uIwWm'
+    b'wBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsCt'
+    b'vRSYAlNgCkyBKTAFpsAUmAJTYArc/00KPIVwaOG2EFJgCkyBKTAFpsAUmAJTYApMgSkwBabA'
+    b'FJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp8N5CSIEp'
+    b'MAWmwBSYAlNgCkyB18tMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsCrqFJgCkyB'
+    b'KTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFHp5fCkyBKTAFpsAU'
+    b'mAJTYApMgSkwBabAFJgCU2AKPOW6F5sCU2AKTIF3enspMAWmwBR4uY4UmAJTYApMgSkwBabA'
+    b'FJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCt/ZSYApMgSkwBabA'
+    b'FJgCU2AKTIEpcP83KfAUwqGF20JIgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAU'
+    b'mAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsB7CyEFpsAUmAJTYApMgSkw'
+    b'BV4vMwWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCr6JKgSkwBabAFJgCU2AKTIEp'
+    b'MAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFHh4fikwBabAFJgCU2AKTIEpMAWmwBSY'
+    b'AlNgCkyBKfCU615sCkyBKTAF3untpcAUmAJT4OU6UmAKTIEpMAWmwBSYAlNgCkyBKTAFpsAU'
+    b'mAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCtzaS4EpMAWmwBSYAlNgCkyBKTAFpsD9'
+    b'36TAUwiHFm4LIQWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSY'
+    b'AlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgC7y2EFJgCU2AKTIEpMAWmwBR4vcwUmAJTYApM'
+    b'gSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCryKKgWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkw'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAlPg4fmlwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAU657'
+    b'sSkwBabAFHint5cCU2AKTIGX60iBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSY'
+    b'AlNgCkyBKTAFpsAUmAJTYApMgSlway8FpsAUmAJTYApMgSkwBabAFJgC93+TAk8hHFq4LYQU'
+    b'mAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgC'
+    b'U2AKTIEpMAWmwBSYAlNgCry3EFJgCkyBKTAFpsAUmAJT4PUyU2AKTIEpMAWmwBSYAlNgCkyB'
+    b'KTAFpsAUmAJTYApMgSnwKqoUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAF'
+    b'psAUmAJTYApMgYfnlwJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAk+57sWmwBSYAlPgnd5e'
+    b'CkyBKTAFXq4jBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgC'
+    b'U2AKTIEpMAWmwK29FJgCU2AKTIEpMAWmwBSYAlNgCtz/TQo8hXBo4bYQUmAKTIEpMAWmwBSY'
+    b'AlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJT'
+    b'YApMgSnw3kJIgSkwBabAFJgCU2AKTIHXy0yBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp'
+    b'MAWmwKuoUmAKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAUe'
+    b'nl8KTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYAo85boXmwJTYApMgXd6eykwBabAFHi5jhSY'
+    b'AlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAK3'
+    b'9lJgCkyBKTAFpsAUmAJTYApMgSlw/zcp8BTCoYXbQkiBKTAFpsAUmAJTYApMgSkwBabAFJgC'
+    b'U2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwHsLIQWm'
+    b'wBSYAlNgCkyBKTAFXi8zBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAKvokqBKTAF'
+    b'psAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUeHh+KTAFpsAUmAJT'
+    b'YApMgSkwBabAFJgCU2AKTIEp8JTrXmwKTIEpMAXe6e2lwBSYAlPg5TpSYApMgSkwBabAFJgC'
+    b'U2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AK3NpLgSkwBabAFJgC'
+    b'U2AKTIEpMAWmwP3fpMBTCIcWbgshBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJT'
+    b'YApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmALvLYQUmAJTYApMgSkwBabA'
+    b'FHi9zBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKvIoqBabAFJgCU2AKTIEpMAWm'
+    b'wBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU+Dh+aXAFJgCU2AKTIEpMAWmwBSYAlNg'
+    b'CkyBKTAFpsBTrnuxKTAFpsAUeKe3lwJTYApMgZfrSIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJT'
+    b'YApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKXBrLwWmwBSYAlNgCkyBKTAFpsAUmAL3'
+    b'f5MCTyEcWrgthBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNg'
+    b'CkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKvLcQUmAKTIEpMAWmwBSYAlPg9TJTYApMgSkw'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKfAqqhSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabA'
+    b'FJgCU2AKTIEpMAWmwBSYAlNgCkyBh+eXAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCT7nu'
+    b'xabAFJgCU+Cd3l4KTIEpMAVeriMFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNg'
+    b'CkyBKTAFpsAUmAJTYApMgSkwBabArb0UmAJTYApMgSkwBabAFJgCU2AK3P9NCjyFcGjhthBS'
+    b'YApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AK'
+    b'TIEpMAWmwBSYAlNgCkyBKfDeQkiBKTAFpsAUmAJTYApMgdfLTIEpMAWmwBSYAlNgCkyBKTAF'
+    b'psAUmAJTYApMgSkwBabAq6hSYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAU'
+    b'mAJTYApMgSkwBR6eXwpMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCjzluhebAlNgCkyBd3p7'
+    b'KTAFpsAUeLmOFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AK'
+    b'TIEpMAWmwBSYArf2UmAKTIEpMAWmwBSYAlNgCkyBKXD/NynwFMKhhdtCSIEpMAWmwBSYAlNg'
+    b'CkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApM'
+    b'gSkwBabAewshBabAFJgCU2AKTIEpMAVeLzMFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWm'
+    b'wBSYAq+iSoEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBR4'
+    b'eH4pMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSnwlOtebApMgSkwBd7p7aXAFJgCU+DlOlJg'
+    b'CkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYArc'
+    b'2kuBKTAFpsAUmAJTYApMgSkwBabA/d+kwFMIhxZuCyEFpsAUmAJTYApMgSkwBabAFJgCU2AK'
+    b'TIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAu8thBSY'
+    b'AlNgCkyBKTAFpsAUeL3MFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYAq8iioFpsAU'
+    b'mAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJT4OH5pcAUmAJTYApM'
+    b'gSkwBabAFJgCU2AKTIEpMAWmwFOue7EpMAWmwBR4p7eXAlNgCkyBl+tIgSkwBabAFJgCU2AK'
+    b'TIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpcGsvBabAFJgCU2AK'
+    b'TIEpMAWmwBSYAvd/kwJPIRxauC2EFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApM'
+    b'gSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYAq8txBSYApMgSkwBabAFJgC'
+    b'U+D1MlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp8CqqFJgCU2AKTIEpMAWmwBSY'
+    b'AlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIGH55cCU2AKTIEpMAWmwBSYAlNgCkyB'
+    b'KTAFpsAUmAJPue7FpsAUmAJT4J3eXgpMgSkwBV6uIwWmwBSYAlNgCkyBKTAFpsAUmAJTYApM'
+    b'gSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsCtvRSYAlNgCkyBKTAFpsAUmAJTYArc'
+    b'/00KPIVwaOG2EFJgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyB'
+    b'KTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp8N5CSIEpMAWmwBSYAlNgCkyB18tMgSkwBabA'
+    b'FJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsCrqFJgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgC'
+    b'U2AKTIEpMAWmwBSYAlNgCkyBKTAFHp5fCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKPOW6'
+    b'F5sCU2AKTIF3enspMAWmwBR4uY4UmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyB'
+    b'KTAFpsAUmAJTYApMgSkwBabAFJgCt/ZSYApMgSkwBabAFJgCU2AKTIEpcP83KfAUwqGF20JI'
+    b'gSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp'
+    b'MAWmwBSYAlNgCkyBKTAFpsB7CyEFpsAUmAJTYApMgSkwBV4vMwWmwBSYAlNgCkyBKTAFpsAU'
+    b'mAJTYApMgSkwBabAFJgCr6JKgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJT'
+    b'YApMgSkwBabAFHh4fikwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKfCU615sCkyBKTAF3unt'
+    b'pcAUmAJT4OU6UmAKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp'
+    b'MAWmwBSYAlNgCtzaS4EpMAWmwBSYAlNgCkyBKTAFpsD936TAUwiHFm4LIQWmwBSYAlNgCkyB'
+    b'KTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkw'
+    b'BabAFJgC7y2EFJgCU2AKTIEpMAWmwBR4vcwUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSY'
+    b'AlNgCryKKgWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlPg'
+    b'4fmlwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAU657sSkwBabAFHint5cCU2AKTIGX60iB'
+    b'KTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSlw'
+    b'ay8FpsAUmAJTYApMgSkwBabAFJgC93+TAk8hHFq4LYQUmAJTYApMgSkwBabAFJgCU2AKTIEp'
+    b'MAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCry3EFJg'
+    b'CkyBKTAFpsAUmAJT4PUyU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSnwKqoUmAJT'
+    b'YApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgYfnlwJTYApMgSkw'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAk+57sWmwBSYAlPgnd5eCkyBKTAFXq4jBabAFJgCU2AKTIEp'
+    b'MAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwK29FJgCU2AKTIEp'
+    b'MAWmwBSYAlNgCtz/TQo8hXBo4bYQUmAKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkw'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSnw3kJIgSkwBabAFJgCU2AK'
+    b'TIHXy0yBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwKuoUmAKTIEpMAWmwBSYAlNg'
+    b'CkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAUenl8KTIEpMAWmwBSYAlNgCkyBKTAF'
+    b'psAUmAJTYAo85boXmwJTYApMgXd6eykwBabAFHi5jhSYAlNgCkyBKTAFpsAUmAJTYApMgSkw'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAK39lJgCkyBKTAFpsAUmAJTYApMgSlw'
+    b'/zcp8BTCoYXbQkiBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAF'
+    b'psAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWmwHsLIQWmwBSYAlNgCkyBKTAFXi8zBabAFJgC'
+    b'U2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAKvokqBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AK'
+    b'TIEpMAWmwBSYAlNgCkyBKTAFpsAUeHh+KTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEp8JTr'
+    b'XmwKTIEpMAXe6e2lwBSYAlPg5TpSYApMgSkwBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAF'
+    b'psAUmAJTYApMgSkwBabAFJgCU2AK3NpLgSkwBabAFJgCU2AKTIEpMAWmwP3fpMBTCIcWbgsh'
+    b'BabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApMgSkwBabAFJgCU2AKTIEpMAWm'
+    b'wBSYAlNgCkyBKTAFpsAUmALvLYQUmAJTYApMgSkwBabAFHi9zBSYAlNgCkyBKTAFpsAUmAJT'
+    b'YApMgSkwBabAFJgCU2AKvIoqBabAFJgCU2AKTIEpMAWmwBSYAlNgCkyBKTAFpsAUmAJTYApM'
+    b'gSkwBabAFJgCU+Dh+aXAFJgCU2AKTIEpMAWmwBSYAlNgCkyBh2Znv38DafWwyFC5X1UAAAAA'
+    b'SUVORK5CYII=')
+
+
+
 
 if platform.system() == "Windows":
     import ctypes
@@ -64,7 +239,27 @@ class VariablePanel(wx.Panel):
 
         self.sizer.AddGrowableCol(0, 1)
         
-        self.SetBackgroundColour(wx.YELLOW)
+        #self.SetBackgroundColour(wx.YELLOW)
+        self.Bind(wx.EVT_PAINT, self._on_paint)
+        self.Bind(wx.EVT_SIZE, self._on_size)
+
+
+    def _on_paint(self, event):
+        dc = wx.PaintDC(self)
+        dc.Clear()
+
+        rect = self.GetClientRect()
+
+        dc.SetPen(wx.GREY_PEN)
+        #dc.SetBrush(wx.RED_BRUSH)
+        dc.SetBrush(wx.TRANSPARENT_BRUSH)
+
+        dc.DrawRectangle(rect)
+
+
+    def _on_size(self, event):
+        self.Refresh()
+        self.sizer.Layout()
 
 
     def _on_edit(self, event):
@@ -295,7 +490,8 @@ class HSVSliders(wx.Panel):
         self.color_preview.SetBackgroundColour(wx.Colour(255, 255, 255))
 
         # Load and scale the hue gradient image
-        gradient_image = wx.Image('images/hue.png', wx.BITMAP_TYPE_PNG)
+        #gradient_image = wx.Image('images/hue.png', wx.BITMAP_TYPE_PNG)
+        gradient_image = wx.Image(hue_image.GetImage())
         gradient_image = gradient_image.Scale(*dip(400, 30))  # Match the slider size        
 
         # Create sliders for H, S, and V
@@ -370,8 +566,6 @@ class EditVariableFrame(wx.Frame):
         self.variableName = variableName
         self.mainFrame = mainFrame
 
-        #self.mainFrame.sourcePanel.pause()
-
         if self.mode == "edit":
             global hsvEditLower, hsvEditUpper
             hsvEditLower = tuple(hsvBounds[self.variableName]["lower"])
@@ -380,15 +574,16 @@ class EditVariableFrame(wx.Frame):
             hsvEditLower = (0, 0, 0)
             hsvEditUpper = (179, 255, 255)
 
-
         self.Bind(wx.EVT_CLOSE, self._on_close)
 
         self._init_gui()
 
+
     def _init_gui(self):
 
         self.SetTitle(f"{self.mode.capitalize()} variable...")
-        self.SetMinClientSize(dip(1200, 700))
+        self.SetMinClientSize(dip(1200, 750))
+        self.SetBackgroundColour(wx.WHITE)
 
         self.sizer = wx.GridBagSizer()
         self.SetSizer(self.sizer)
@@ -424,9 +619,11 @@ class EditVariableFrame(wx.Frame):
         self.buttonOk.Bind(wx.EVT_BUTTON, self._on_ok)
         self.buttonCancel.Bind(wx.EVT_BUTTON, self._on_cancel)
 
+
     def _on_close(self, event):
         #self.mainFrame.sourcePanel.resume()
-        self.Close()
+        self.Destroy()
+
 
     def _on_ok(self, event):
         global hsvBounds, activeMasks
@@ -445,6 +642,7 @@ class EditVariableFrame(wx.Frame):
 
         self.sourcePanel.pause()
         self.Destroy()
+
 
     def _on_cancel(self, event):
         self.sourcePanel.pause()
@@ -471,13 +669,10 @@ class MainFrame(wx.Frame):
         self.mainPanel = wx.Panel(self)
         self.mainPanelSizer = wx.GridBagSizer()
         self.mainPanel.SetSizer(self.mainPanelSizer)
-        self.mainPanel.SetBackgroundColour(wx.BLUE)
-
 
         # ---------------------- left panel ------------------------ #
 
         self.leftPanel = wx.Panel(self.mainPanel)
-        self.leftPanel.SetBackgroundColour(wx.GREEN)
         self.leftPanelSizer = wx.BoxSizer(wx.VERTICAL)
         self.leftPanel.SetSizer(self.leftPanelSizer)
         self.scrolledPanel = wx.ScrolledWindow(self.leftPanel)
@@ -491,21 +686,17 @@ class MainFrame(wx.Frame):
         # ---------------------- right panel ------------------------ #
 
         self.rightPanel = wx.Panel(self.mainPanel)
-        self.rightPanel.SetBackgroundColour(wx.CYAN)
         self.rightPanelSizer = wx.BoxSizer(wx.VERTICAL)
         self.rightPanel.SetSizer(self.rightPanelSizer)
-        #self.sourcePanel = SourcePanel(self.rightPanel, source="test.mp4", showMask=True, img_size=(1100, 900))
-        self.sourcePanel = SourcePanel(self.rightPanel, source="images/hue.png", showMask=True, img_size=(1100, 900))
+        self.sourcePanel = SourcePanel(self.rightPanel, source="shapes.png", showMask=True, img_size=(1100, 900))
+        #self.sourcePanel = SourcePanel(self.rightPanel, source="images/hue.png", showMask=True, img_size=(1100, 900))
         self.rightPanelSizer.Add(self.sourcePanel, 1, wx.EXPAND)
-
-        #self.rightPanelSizer.Add(wx.RadioButton(self.rightPanel, label="test"), 1, wx.EXPAND)
 
         # ---------------------- add panels to sizer ------------------------ #
 
         self.mainPanelSizer.Add(self.leftPanel, pos=(0, 0), flag=wx.EXPAND)
         self.mainPanelSizer.Add(self.rightPanel, pos=(0, 1), flag=wx.EXPAND)
         self.mainPanelSizer.AddGrowableCol(0, 1)
-        # self.mainPanelSizer.AddGrowableCol(1, 1)
         self.mainPanelSizer.AddGrowableRow(0, 1)
 
         self.mainPanelSizer.Layout()
@@ -573,6 +764,7 @@ class MainFrame(wx.Frame):
         frame = EditVariableFrame(self, "add", "", self)
         frame.Show()
         #self._update_variable_panels()
+
 
     def _on_close(self, event):
         self.sourcePanel.pause()
